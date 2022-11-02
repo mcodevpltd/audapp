c81c16076443257633bb31e0394ca5b360e10cb3
sarath@gmail.com <sarath@gmail.com>
2022-11-03 03:45:48 +0530

    changes in base entity

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -7,8 +7,7 @@ export abstract class BaseEntity {
     @Column({ type: 'boolean', default: true })
     isActive?: boolean;
 
-    @Column({ type: 'boolean', default: false })
-    isArchived?: boolean;
+   
 
     @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
     createDateTime?: Date;
@@ -16,15 +15,10 @@ export abstract class BaseEntity {
     @Column({ type: 'varchar', length: 300,nullable:true })
     createdBy?: string;
 
-    @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
-    lastChangedDateTime?: Date;
-
-    // @Column({ type: 'varchar', length: 300 })
-    // lastChangedBy: string;
 
     @DeleteDateColumn({ type: 'timestamptz',nullable: true })
     deletedDateTime?: Date;
     
     @Column({ type: 'varchar', length: 300, nullable: true })
     internalComment?: string | null;
-}
+} 
